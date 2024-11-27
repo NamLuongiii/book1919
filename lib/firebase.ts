1b9132647803154d78354a164294174a83a68ffa
luong_nam <namlk@solashi.com>
2024-11-27 09:55:31 +0700

    ebb

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,10 +1,23 @@
 "server-only";
 
 import admin from "firebase-admin";
-import { getApps, initializeApp } from "firebase-admin/app";
+import { getApps, initializeApp, ServiceAccount } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getDownloadURL, getStorage } from "firebase-admin/storage";
-var serviceAccount = require("./fb-key.json");
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: "googleapis.com",
+} as ServiceAccount;
 
 const yourFirebaseAdminConfig = {
   credential: admin.credential.cert(serviceAccount),
