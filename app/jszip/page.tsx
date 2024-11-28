22e4ecae048ba432adea23cab994f9dd1494501a
luong_nam <namlk@solashi.com>
2024-11-28 17:15:34 +0700

    jszip

---
+++ app/jszip/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import JSZip from "jszip";
+import { useEffect } from "react";
+
+interface Props {}
+
+const url = "https://s3.amazonaws.com/moby-dick/moby-dick.epub";
+
+export default function JsZip(props: Props) {
+  useEffect(() => {
+    load();
+  });
+
+  const load = async () => {
+    const res = await fetch(url);
+    const blob = await res.blob();
+
+    // Đọc file EPUB
+    const epub = await JSZip.loadAsync(blob);
+
+    console.log(epub.files);
+
+    const content = await epub.file("OPS/chapter_005.xhtml")?.async("string");
+    const test_ele = document.getElementById("test");
+    if (test_ele) test_ele.innerHTML = content ?? "";
+  };
+
+  return <div id="test">hello</div>;
+}
